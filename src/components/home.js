6ad1beceac74fb05da9a61905255c74aec2a124d
TrushalPatel0 <business.trushalpatel@gmail.com>
2024-09-28 19:10:16 +0530

    CHANGES DONE

--- src/components/home.js
+++ src/components/home.js
@@ -10,11 +10,48 @@ const Home = () => {
  
             <h1 className='pagetitle'>Home</h1>
             <br/>
-                <div className='card'>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-3'>
+                <div className='card home hover:bg-blue-100'>
                     <div className='card-body'>
-                        This is Home Page
+                    <div className='grid grid-cols-3 gap-3'>
+                          <div className='text-3xl d-flex justify-content-center align-items-center col-span-1 text-blue-400'><i class="fa-solid fa-hand-holding-dollar"></i></div>
+                          <div className='col-span-2'><div className='text-gray-500'>Total Sales</div>
+                          <div className='mt-2'><div className='text-xl'><i class="fa-solid fa-indian-rupee-sign"></i>5000</div></div></div>
+                    </div>   
                     </div>
                 </div>
+
+                <div className='card home hover:bg-blue-100'>
+                    <div className='card-body'>
+                    <div className='grid grid-cols-3 gap-3'>
+                          <div className='text-3xl d-flex justify-content-center align-items-center col-span-1 text-blue-400'><i class="fa-solid fa-dolly"></i></div>
+                          <div className='col-span-2'><div className='text-gray-500'>Total Orders</div>
+                          <div className='mt-2'><div className='text-xl'>350</div></div></div>
+                    </div>   
+                    </div>
+                </div>
+
+                <div className='card home hover:bg-blue-100'>
+                    <div className='card-body'>
+                    <div className='grid grid-cols-3 gap-3'>
+                          <div className='text-3xl d-flex justify-content-center align-items-center col-span-1 text-blue-400'><i class="fa-solid fa-users"></i></div>
+                          <div className='col-span-2'><div className='text-gray-500'>Total Customers</div>
+                          <div className='mt-2'><div className='text-xl'>120</div></div></div>
+                    </div>   
+                    </div>
+                </div>
+
+                <div className='card home hover:bg-blue-100'>
+                    <div className='card-body'>
+                    <div className='grid grid-cols-3 gap-3'>
+                          <div className='text-3xl d-flex justify-content-center align-items-center col-span-1 text-blue-400'><i class="fa-solid fa-eye"></i></div>
+                          <div className='col-span-2'><div className='text-gray-500'>Total Visitors</div>
+                          <div className='mt-2'><div className='text-xl'>5000</div></div></div>
+                    </div>   
+                    </div>
+                </div>
+
+            </div>
           
 
   
