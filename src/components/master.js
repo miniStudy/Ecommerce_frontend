e63fe34acf72929ef7e3d7cd2516a8cb79e1de52
Shivxsks <shivksharma0904@gmail.com>
2024-09-28 18:44:20 +0530

    Changes

--- src/components/master.js
+++ src/components/master.js
@@ -31,6 +31,7 @@ import ShowOrder from './show_orders.js';
 import OrderDetails from './show_order_details.js';
 import ShowStock from './show_stock.js';
 import ShowStockManage from './show_stock_manage.js';
+import ShowUsers from './show_users.js';
 
 function Master() {
   return (
@@ -57,15 +58,15 @@ function Master() {
             <Route path="/admin/add_category" element={<AddCategory />} />
             <Route path="/admin/update_category/:categoryId/" element={<UpdateCategory />} />
 
-            <Route path="/admin/Brands" element={<Show_Brands />} />
+            <Route path="/admin/brands" element={<Show_Brands />} />
             <Route path="/admin/add_brand" element={<AddBrand />} />
             <Route path="/admin/update_brand/:brandId/" element={<UpdateBrand />} />
 
-            <Route path="/admin/Sizes" element={<Show_Sizes />} />
+            <Route path="/admin/sizes" element={<Show_Sizes />} />
             <Route path="/admin/add_size" element={<AddSize />} />
             <Route path="/admin/update_size/:sizeId/" element={<UpdateSize />} />
 
-            <Route path="/admin/Colors" element={<Show_Color />} />
+            <Route path="/admin/colors" element={<Show_Color />} />
             <Route path="/admin/add_colors" element={<AddColor />} />
             <Route path="/admin/update_color/:colorId/" element={<UpdateColors />} />
 
@@ -73,11 +74,13 @@ function Master() {
             <Route path="/admin/update_offer/:offerId/" element={<UpdateOffer />} />
             <Route path="/admin/add_offer" element={<AddOffer />} />
 
-            <Route path="/admin/Orders" element={<ShowOrder />} />
-            <Route path="/admin/Order_Details/:order_id/" element={<OrderDetails />} />
+            <Route path="/admin/orders" element={<ShowOrder />} />
+            <Route path="/admin/order_details/:order_id/" element={<OrderDetails />} />
 
-            <Route path="/admin/Stocks" element={<ShowStock />} />
+            <Route path="/admin/stocks" element={<ShowStock />} />
             <Route path="/admin/stock_management" element={<ShowStockManage />} />
+
+            <Route path="/admin/users" element={<ShowUsers />} />
 
           {/* <Route path="/admin/offer_details/:offerId/" element={<OfferDetails />} /> */}
             </Routes>
