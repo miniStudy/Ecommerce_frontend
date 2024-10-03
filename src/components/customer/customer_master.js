49fea04e9b861bcf0664c921e79e854143673f83
TrushalPatel0 <business.trushalpatel@gmail.com>
2024-10-03 13:17:35 +0530

    changes done

--- src/components/customer/customer_master.js
+++ src/components/customer/customer_master.js
@@ -6,7 +6,6 @@ import CustomerLayout from './customer_layout';
 import CustomerHome from './customer_home';
 import ShowProducts from './customer_product';
 import CustomerCart from './customer_cart';
-import Home from './home';
 
 
 import CustomerWhishlist from './customer_whishlist';
@@ -21,7 +20,6 @@ function Customer_Master() {
       <Route path="/" element={<CustomerLayout><CustomerHome /></CustomerLayout>} />
       <Route path="/show_products" element={<CustomerLayout><ShowProducts /></CustomerLayout>} />
       <Route path="/cart" element={<CustomerLayout><CustomerCart /></CustomerLayout>} />
-      <Route path="/homee" element={<CustomerLayout><Home /></CustomerLayout>} />
 
 
 
